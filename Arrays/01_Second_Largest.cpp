8417175183097a41823bb8903455e454428c1d4d
SHASHANK-U-04 <shashank.u.shashu1359@gmail.com>
2026-01-21 20:33:03 +0530

    Initial setup: Added folder structure, README, and Second Largest element solution

---
+++ Arrays/01_Second_Largest.cpp
@@ -0,0 +1,22 @@
+﻿#include <limits.h>
+
+int getSecondLargest(int *arr, int n) {
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    // If second largest doesn't exist
+    if (secondLargest == INT_MIN)
+        return -1;
+
+    return secondLargest;
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# GFG 160 Problems
+
+This repository contains solutions to **GeeksforGeeks 160 DSA problems**, organized topic-wise.
+
+## Folder Structure
+- Arrays
+- Strings
+- Linked List
+- Stack
+- Queue
+- Trees
+- Graphs
+- Dynamic Programming
+
+## Language Used
+- C++
+
+## Goal
+- Strengthen DSA fundamentals
+- Prepare for coding interviews
+- Maintain clean, readable, and optimized solutions
